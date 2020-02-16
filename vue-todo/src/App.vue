4b4d1038ab3c74179aa91fc2ea54157d7746fa4c
in-kim <ehddls960617@naver.com>
2020-02-16 22:09:42 +0900

    오늘의 할일앱 프로젝트 시작

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/Todoinput.vue'
+import TodoList from './components/Todolist.vue'
+import TodoFooter from './components/Todofooter.vue'
+
+export default {
+  components : {
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter,
+  }
+}
+</script>
+
+<style>
+  body {text-align:center; background-color:#F6F6F6}
+  input {border-style : groove; width:200px;}
+  button { border-style: grrove;}
+  .shadow {box-shadow : 5px 10px 10px rgba(0,0,0.03);}
+</style> 
---
+++ vue-todo/src/components/Todoinput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <input type="text" v-model="newTodoItem">
+      <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+   data : function() {
+       return {
+           newTodoItem : ""
+       }
+   },
+   methods: {
+       addTodo : function() {
+           // localstorage mdn
+           // 저장하는 로직
+           localStorage.setItem(this.newTodoItem, this.newTodoItem);
+           // 확인하는 방법 
+           // 브라우저 개발자 모드 -> application -> storage -> Local Storage 확인
+
+           // 초기화 로직
+           
+           this.newTodoItem = '';
+       }
+   }
+}
+</script>
+
+<style>
+
+</style>
