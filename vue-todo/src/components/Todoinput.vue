974c17ec22e421dbfb450aab98ddaee47ef93873
in-kim <ehddls960617@naver.com>
2020-02-17 13:07:50 +0900

    input onkeyup enter 기능 추가 및 input 스타일링

--- vue-todo/src/components/Todoinput.vue
+++ vue-todo/src/components/Todoinput.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-      <input type="text" v-model="newTodoItem">
-      <button v-on:click="addTodo">add</button>
+  <div class="inputBox shadow">
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+      <span class="addContainer">
+          <i class="fas fa-plus addBtn" v-on:click="addTodo"></i>
+      </span>
   </div>
 </template>
 
@@ -13,21 +15,28 @@ export default {
        }
    },
    methods: {
+       // 저장하는 로직
        addTodo : function() {
            // localstorage mdn
-           // 저장하는 로직
            localStorage.setItem(this.newTodoItem, this.newTodoItem);
            // 확인하는 방법 
            // 브라우저 개발자 모드 -> application -> storage -> Local Storage 확인
 
-           // 초기화 로직
-           
+           // input 초기화 로직 호출
+           this.clearInput();
+       },
+       // 초기화 로직
+       clearInput : function(){
            this.newTodoItem = '';
        }
    }
 }
 </script>
 
-<style>
-
+<style scrope>
+ input:focus {outline:none;}
+ .inputBox {background:#fff; height:50px; line-height:50px; border-radius:5px;}
+ .inputBox input {border-style:none; font-size:0.9rem;}
+ .addContainer {float:right; background: linear-gradient(to right, #6478FB, #8763Fb); display:block; width:3rem; border-radius:0 5px 5px 0;}
+ .addBtn { color:#fff; vertical-align:middle; cursor:pointer;}
 </style>
