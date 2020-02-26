a652b0c5482c0f0e30f6ce0001188c0124217934
in-kim <ehddls960617@naver.com>
2020-02-26 12:42:58 +0900

    vuex 설치 및 설정

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// store 변수를 사용하기 위해 {}로 감싸줌.
+import { store } from './store/store.js'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    // 축약
+    store,
+    render: h => h(App)
 })
---
+++ vue-todo/src/store/store.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// vue plugin 사용
+// use 를 사용하면 plugin을 전역으로 사용할 수 있다.
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    //
+})
